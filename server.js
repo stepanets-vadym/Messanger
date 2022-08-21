b41cc2342a19637289c70700ee417c6bec68bac4
stepanets-vadym <alkatras427@gmail.com>
2022-08-22 01:03:39 +0300

    test3

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./server/db.json');
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
   static: './build'
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 server.use(middlewares);
 server.use(jsonServer.rewriter({
   '/api/*': '/$1',
