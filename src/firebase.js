ac500e3aeed3596d7d8676b1584323ea55cd6bef
stepanets-vadym <alkatras427@gmail.com>
2022-08-22 01:17:03 +0300

    test22

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,11 @@ import { initializeApp } from 'firebase/app';
 
 
 export const firebaseConfig = {
-
+  apiKey: 'AIzaSyCwR4Jso91DK04eRXMtJHfhx9msEP_OopE',
+  authDomain: 'chat-react-21623.firebaseapp.com',
+  projectId: 'chat-react-21623',
+  storageBucket: 'chat-react-21623.appspot.com',
+  messagingSenderId: '452500312548',
+  appId: '1:452500312548:web:f348fadca32a4e0bbf37f1',
 };
 export const app = initializeApp(firebaseConfig);
